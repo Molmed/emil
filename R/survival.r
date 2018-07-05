bf1bbaa7bafb3b1bd903d40cf4becc59fa94242c
Christofer Bäcklin <christofer.backlin@formulate.se>
2018-07-05 08:53:01 +0200

    add rendered version of manual

--- R/survival.r
+++ R/survival.r
@@ -159,10 +159,12 @@ Surv_event_types <- function(x){
     if(mstat) attr(x, "states") else c("no event", "event")
 }
 
-#' Plot Surv vector
+#' Plot Surv vector [DEPRECATED]
 #'
-#' @method plot Surv
-#' @param x \code{\link{Surv}} vector.
+#' Package \pkg{survival} includes an official plot function as of version
+#' 2.42-5. This will therefore be removed in the next major update.
+#'
+#' @param x \code{\link[survival]{Surv}} vector.
 #' @param y Y-values.
 #' @param segments Whether to draw horizontal segments.
 #' @param flip Flip the plot to show time on y.
@@ -172,7 +174,9 @@ Surv_event_types <- function(x){
 #' @author Christofer \enc{Bäcklin}{Backlin}
 #' @importFrom graphics legend points
 #' @export
-plot.Surv <- function(x, y, segments=TRUE, flip=FALSE, legendpos="topright", ...){
+plot_Surv <- function(x, y, segments=TRUE, flip=FALSE, legendpos="topright", ...){
+    message("`plot_Surv` is deprecated, please use the regular `plot` function (now dispatching to `surivival:::plot.Surv`")
+
     if(missing(y)) y <- 1:length(x)
 
     if(flip){
